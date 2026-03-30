8fef890d3c25a96589ddb34b51696876130aadd1
Abiola Abolaji <abiolaabeeola159@gmail.com.com>
2026-03-30 12:10:08 +0100

    implement navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
 
 const interHeading = Inter({subsets:['latin'],variable:'--font-heading'});
 
@@ -30,9 +31,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", montserrat.variable, interHeading.variable)}
+      className={cn("h-full", "antialiased", "dark", geistSans.variable, geistMono.variable, "font-sans", montserrat.variable, interHeading.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }

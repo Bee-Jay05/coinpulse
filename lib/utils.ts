505a80db5f3f4bfaac529f1449cc3c06f3cfe8a2
Abiola Abolaji <abiolaabeeola159@gmail.com.com>
2026-03-31 22:55:45 +0100

    implement coin overview and trending coins ui and functionality using the fetcher utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(
+  value: number,
+  currency: string = "USD",
+  decimals = 2
+) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(value);
+}
